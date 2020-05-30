db53b8d91062d50792c00c38ba35cc15f336c1fc
EuclidesXcode <euclideslione@gmail.com>
2020-05-29 22:44:41 -0300

    created new layout from dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -19,66 +15,83 @@
             <v-icon>mdi-cog</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+            <v-list-item-title>Produtos</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-Account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Usuarios</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Configurações</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      clipped-left
-    >
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>myMarket</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    <div class="d-flex align-center">
+      <v-switch 
+        style="margin-top: 15px"
+        v-model="ChangeTheme" 
+        inset
+        :v-on="chengeTheme()"
+      ></v-switch>
+    </div>
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
+      <SalesUser />
     </v-content>
 
     <v-footer app>
-      <span> myMarket &copy; 2020</span>
+      <span>myMarket &copy; 2020</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-      setTheme: false
-    }),
-    created () {
-      this.$vuetify.theme.dark = this.setTheme
-    },
+import SalesUser from "./components/SalesUser";
+
+export default {
+  props: {
+    source: String
+  },
+  components: {
+    SalesUser: SalesUser
+  },
+  data: () => ({
+    drawer: null,
+    ChangeTheme: false,
+  }),
+  created() {
+      this.$vuetify.theme.dark = this.ChangeTheme;
+  },
+  computed: {
+  },
+  methods: {
+    chengeTheme() {
+      if(this.ChangeTheme === false) {
+        this.$vuetify.theme.dark = true
+      } else {
+        this.$vuetify.theme.dark = false
+      }
+    }
   }
+};
 </script>
---
+++ src/components/ListSalesUser.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Tipo de Pagamento',
+            align: 'start',
+            sortable: false,
+            value: 'typePayment',
+          },
+          { text: 'Valor da Venda', value: 'sale' },
+          { text: 'Comissão', value: 'commission' },
+          { text: 'Desconto', value: 'discount' }
+        ],
+        desserts: [
+          {
+            typePayment: 'CREDITO | MASTER',
+            sale: 'R$ 239,99',
+            commission: '5%',
+            discount: '10%',
+          },
+          {
+            typePayment: 'DEBITO | VISA',
+            sale: 'R$ 89,99',
+            commission: '15%',
+            discount: '0%',
+          },
+          {
+            typePayment: 'CREDITO | ELO',
+            sale: 'R$ 3.239,99',
+            commission: '25%',
+            discount: '0%',
+          },
+          {
+            typePayment: 'CREDITO | MASTER',
+            sale: 'R$ 39,99',
+            commission: '5%',
+            discount: '10%',
+          },
+        ],
+      }
+    },
+  }
+</script>
