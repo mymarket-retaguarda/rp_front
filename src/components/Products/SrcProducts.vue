4719fbb78dda2c804f43873d3d3f8139a5ccdcb3
EuclidesXcode <euclideslione@gmail.com>
2020-06-02 13:07:00 -0300

    Change Products

--- src/components/Products/SrcProducts.vue
+++ src/components/Products/SrcProducts.vue
@@ -1,123 +1,136 @@
 <template>
-  <v-card>
-    <v-card-title>
-        Consulta de Produtos
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Buscar produto por nome, código de barras ou código interno..."
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-  </v-card>
+  <v-data-table :headers="headers" :items="desserts" sort-by="quant" class="elevation-1">
+    <template v-slot:item.quant="{ item }">
+      <v-chip :color="getColor(item.quant)" dark>{{ item.quant }}</v-chip>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
+export default {
+  data: () => ({
+    dialog: false,
+    headers: [
+      { text: "EAN13", value: "ean" },
+      { text: "Produto", value: "productName" },
+      { text: "Estoque", value: "quant" },
+      { text: "Preço", value: "price" },
+      { text: "Ações", value: "actions", sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
     },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    }
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          productName: "Coca-Cola 2LT",
+          ean: 789564656163,
+          quant: 22,
+          price: "R$ 6,99"
+        },
+        {
+          productName: "Guaraná Kuat 2LT",
+          ean: 789564656547,
+          quant: 5,
+          price: "R$ 4,99"
+        },
+        {
+          productName: "Suco Tang Uva UND",
+          ean: 789564634444,
+          quant: 350,
+          price: "R$ 0,99"
+        },
+        {
+          productName: "Suco del valle 250ml",
+          ean: 789564656163,
+          quant: 32,
+          price: "R$ 2,69"
+        },
+        {
+          productName: "Suco Laranja Del Valle Fresh 200ml",
+          ean: 789564621555,
+          quant: 22,
+          price: "R$ 1,29"
+        },
+        {
+          productName: "Mineirinho 2LT",
+          ean: 789564656222,
+          quant: 12,
+          price: "R$ 3,99"
+        },
+      ];
+    },
+
+    getColor (quant) {
+        if (quant < 10) return 'red'
+        else if (quant < 20 ) return 'orange'
+        else return 'green'
+      },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.desserts.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    }
   }
+};
 </script>
