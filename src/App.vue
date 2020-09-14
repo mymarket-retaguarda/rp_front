f7fd24bbecea23a293773253761cc85ddafa8dbb
EuclidesXcode <euclideslione@gmail.com>
2020-09-14 10:38:39 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 
           <v-list-item-content>
             <v-list-item-title>Euclides Nascimento</v-list-item-title>
-            <v-list-item-subtitle>Gerente Adm...</v-list-item-subtitle>
+            <v-list-item-subtitle>Gerente Adm</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
 
